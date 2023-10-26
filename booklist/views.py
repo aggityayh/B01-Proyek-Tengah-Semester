db95e08ba76a27f9ff2cc1d6d5c372d66f47a997
Muhammad Hanif <muhammadhanif2404@gmail.com>
2023-10-26 23:35:14 +0700

    Mengubah Models sama seperti Models di pengelola

--- booklist/views.py
+++ booklist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from booklist.models import Book
+from pengelola.models import Buku
 from django.core import serializers
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,7 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def show_booklist(request):
-    books = Book.objects.all()
+    books = Buku.objects.all()
 
     context = {
         'nameapp': 'AncestralReads',
@@ -22,7 +23,7 @@ def get_book_json(request):
 @csrf_exempt
 def add_book_ajax(request):
     if request.method == 'POST':
-        book_temp = Book.objects.get(pk=request.POST.get("buku"))
+        book_temp = Buku.objects.get(pk=request.POST.get("buku"))
         new_book = Book(text_number=book_temp.text_number, title=book_temp.title, language=book_temp.language, first_name=book_temp.first_name, last_name=book_temp.last_name, year=book_temp.year, subjects=book_temp.subjects, bookshelves=book_temp.bookshelves)
         new_book.save()
         
