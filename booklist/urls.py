cc39530a68cc98d520a70554ce2c80a4043bafcc
Muhammad Hanif <muhammadhanif2404@gmail.com>
2023-10-28 12:20:53 +0700

    Menambah Delete Button, Handle masalah duplikat buku

--- booklist/urls.py
+++ booklist/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from booklist.views import show_booklist, get_book_json, add_book_ajax
+from booklist.views import show_booklist, get_book_json, get_buku_json, add_book_ajax, delete_book_ajax
 
 app_name = 'booklist'
 
 urlpatterns = [
     path('', show_booklist, name='show_booklist'),
     path('get-book/', get_book_json, name='get_book_json'),
+    path('get-buku/', get_buku_json, name='get_buku_json'),
     path('create-book-ajax/', add_book_ajax, name='add_book_ajax'),
+    path('delete-book-ajax/', delete_book_ajax, name='delete_book_ajax'),
 ]
--- booklist/views.py
+++ booklist/views.py
@@ -7,12 +7,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 
-@login_required(login_url='pengelola/login/')
+@login_required(login_url='/login/')
 def show_booklist(request):
-    books = Buku.objects.all()
+    title_list = Book.objects.values_list('title', flat=True)
+    books = Buku.objects.exclude(title__in=title_list)
 
     context = {
         'nameapp': 'AncestralReads',
@@ -24,6 +27,11 @@ def get_book_json(request):
     book_item = Book.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize('json', book_item))
 
+def get_buku_json(request):
+    title_list = Book.objects.values_list('title', flat=True)
+    buku_item = Buku.objects.exclude(title__in=title_list)
+    return HttpResponse(serializers.serialize('json', buku_item))
+
 @csrf_exempt
 def add_book_ajax(request):
     if request.method == 'POST':
@@ -32,5 +40,19 @@ def add_book_ajax(request):
         new_book.save()
         
         return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def delete_book_ajax(request):
+    if request.method == "DELETE":
+        try:
+            id = json.loads(request.body.decode('utf-8')).get('pk')
+            book_temp = Book.objects.get(pk=id)
+            book_temp.delete()
+            
+            return HttpResponse(b"DELETED", status=201)
+        except:
+            return HttpResponse(b"Not Found", status=201)
 
     return HttpResponseNotFound()
