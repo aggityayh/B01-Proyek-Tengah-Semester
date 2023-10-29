eb4a64eb01e5d45596bd682bf49163b481eb30ef
Muhammad Hanif <muhammadhanif2404@gmail.com>
2023-10-29 22:51:23 +0700

    Percobaan Deploy yang ke-1

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from caturapp.models import Pustakawan
-
-# Create your models here.
-# class Book(models.Model):
-#     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=255, default='')
-#     year = models.IntegerField(default=0)
 
 class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
