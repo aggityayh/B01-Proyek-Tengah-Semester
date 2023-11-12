e5218870c41ef39916c5a5a51bfeabd9cf7e869a
aggityayh <agitbarat@gmail.com>
2023-11-12 14:04:56 +0700

    Ada error utk demo sedikit

--- b01project/urls.py
+++ b01project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('request_book.urls')),
+    path(''),
     path('request_book/', include('request_book.urls')),
     path('admin/', admin.site.urls),
     path('pengelola/', include('pengelola.urls')),
